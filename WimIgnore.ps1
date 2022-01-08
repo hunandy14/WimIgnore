ccd8200e2fca908a3a5d3840457eb975b4980a1e
Charlotte.Hong <hunandy14@gmail.com>
2022-01-08 19:20:14 +0900

    update

--- WimIgnore.ps1
+++ WimIgnore.ps1
@@ -1,7 +1,18 @@
 function WimIgnore {
     param (
         [Parameter(Position = 0, ParameterSetName = "", Mandatory=$true)]
-        [string] $DriveLetter
+        [string] $DriveLetter,
+        [string] $Out
     )
-    
+    $ignore = irm https://bit.ly/3qXkbkE
+    $onedrive = (Get-ChildItem "$($DriveLetter):\Users" -Dir | ForEach-Object {
+        Get-ChildItem $_.FullName -Dir -Filter:"Onedrive*"
+    })
+    $onedrive.FullName | ForEach-Object {
+        $path=$_ -replace ("$DriveLetter`:", "")
+        $ignore = $ignore + "$path`n"
+    }
+    if (!$Out) { $Out = "WimScript.ini" }
+    [System.IO.File]::WriteAllLines($Out, $ignore);
 }
+# WimIgnore C
